5eac508693b300b3e9ae59f44416eac4800fea5d
Seb Fournier <seb.fournier.tech@proton.me>
2026-02-14 21:19:04 +0100

    Changing color in Home and navbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
                     <Row className="align-items-center">
                         <Col md={7} className="home-header">
                             <h1 style={{ paddingBottom: 15 }} className="heading">
-                                Hi There!{" "}
+                                Hello There!{" "}
                                 <span className="wave" role="img" aria-labelledby="wave">
                                     👋🏻
                                 </span>
