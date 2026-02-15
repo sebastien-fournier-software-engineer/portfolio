4cc6fe51ed607fba6e043dbdd905f7196051db7e
Seb Fournier <seb.fournier.tech@proton.me>
2026-02-15 12:33:27 +0100

    Aesthetic changes in home section

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -2,23 +2,23 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 function Type() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "Robotics Engineer",
-          "Software Architect",
-          "Machine Learning Engineer",
-          "Freelancer",
-          "Open Source Contributor",
-        ],
-        autoStart: true,
-        loop: true,
-        delay: 40,
-        deleteSpeed: 30,
-      }}
-    />
-  );
+    return (
+        <Typewriter
+            options={{
+                strings: [
+                    "Robotics Engineer",
+                    "Machine Learning & AI Expert",
+                    "Freelancer",
+                    "Software Architect",
+                    "Open Source Contributor",
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 40,
+                deleteSpeed: 30,
+            }}
+        />
+    );
 }
 
 export default Type;
